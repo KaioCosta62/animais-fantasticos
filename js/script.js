330a45c7f711f23886150ae3e177d84a19755f9e
KaioCosta62 <kaiohenriqueranking1@gmail.com>
2023-09-10 23:40:16 -0300

    abstrando feature para funcoes isoladas

--- js/script.js
+++ js/script.js
@@ -1,39 +1,49 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li")
-const tabConteudos = document.querySelectorAll(".js-tabconteudo section")
-const verificarItens = tabMenu.length > 0 && tabConteudos.length > 0
+function initNavegacaoTab() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabConteudos = document.querySelectorAll(".js-tabconteudo section");
+  const verificarItens = tabMenu.length > 0 && tabConteudos.length > 0;
 
-if (verificarItens) {
-  tabConteudos[0].classList.add("ativo")
+  if (verificarItens) {
+    tabConteudos[0].classList.add("ativo");
 
-  function exibirConteudo(index) {
-    tabConteudos.forEach((conteudo) => {
-      conteudo.classList.remove("ativo")
-    })
-    tabConteudos[index].classList.add("ativo")
+    function exibirConteudo(index) {
+      tabConteudos.forEach((conteudo) => {
+        conteudo.classList.remove("ativo");
+      });
+      tabConteudos[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((menuItem, index) => {
+      menuItem.addEventListener("click", function () {
+        exibirConteudo(index);
+      });
+    });
   }
-
-  tabMenu.forEach((menuItem, index) => {
-    menuItem.addEventListener("click", function () {
-      exibirConteudo(index)
-    })
-  })
 }
 
+initNavegacaoTab()
 
 /* Accordion */
 
-const perguntasFaq = document.querySelectorAll('.faq-lista dt')
-const respostasFaq = document.querySelectorAll('.faq-lista dd')
-const verificarItensAcordion = perguntasFaq.length > 0 && respostasFaq.length > 0
+function initAccordion(){
+  const perguntasFaq = document.querySelectorAll(".faq-lista dt");
+  const verificarItensAcordion = perguntasFaq.length > 0;
 
-if(verificarItensAcordion){
-  function exibirRespostaAccordion(e){
-    const resposta = e.target.nextElementSibling
-    resposta.classList.toggle('ativo')
-    e.target.classList.toggle('ativo')
+  perguntasFaq[0].classList.add("ativo");
+  perguntasFaq[0].nextElementSibling.classList.add("ativo");
+
+  if (verificarItensAcordion) {
+    function exibirRespostaAccordion(e) {
+      const resposta = e.target.nextElementSibling;
+      resposta.classList.toggle("ativo");
+      e.target.classList.toggle("ativo");
+    }
+
+    perguntasFaq.forEach((pergunta) => {
+      pergunta.addEventListener("click", exibirRespostaAccordion);
+    });
   }
 
-  perguntasFaq.forEach((pergunta) => {
-    pergunta.addEventListener('click', exibirRespostaAccordion)
-  })
 }
+
+initAccordion()
