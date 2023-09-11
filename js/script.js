29897e0568e53806aae91b6f4ebd0d5595d94493
KaioCosta62 <kaiohenriqueranking1@gmail.com>
2023-09-11 18:36:12 -0300

    criando animacao ao scroll

--- js/script.js
+++ js/script.js
@@ -71,3 +71,29 @@ function initScrollSuave(){
 }
 
 initScrollSuave()
+
+
+/* Animação ao scroll */
+
+function initAnimacaoScroll(){
+  
+  function animacaoScroll(){
+    const sections = document.querySelectorAll('.js-section-scroll')
+
+    if(sections){
+      sections.forEach((section) => {
+        const metadeAltura = window.innerHeight * 0.6
+        const distanciaSectionTopo = section.getBoundingClientRect().top - metadeAltura
+        console.log(section.offsetTop)
+        const exibirItem = distanciaSectionTopo < 0
+        if(exibirItem){
+          section.classList.add('ativo')
+        }
+      })
+    }
+  }
+
+  window.addEventListener('scroll', animacaoScroll)
+}
+
+initAnimacaoScroll()
